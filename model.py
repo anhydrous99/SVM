f4a9c68466c09afb0622ec908ce72b73d1da709e
Armando Herrera <anhydrous99@gmail.com>
2019-10-12 20:26:02 -0500

    Got it working!

--- model.py
+++ model.py
@@ -4,35 +4,41 @@ import numpy as np
 from tqdm import tqdm
 
 class SVM:
-    def __init__(self, input_size, epsilon):
+    def __init__(self, input_size, epsilon, train_mode=True):
         self.input_size = input_size
-        self.epsilon = epsilon
+        self.epsilon = torch.tensor(epsilon)
 
-        self.w = torch.rand(input_size, requires_grad=True)
-        self.b = torch.rand(1, requires_grad=True)
+        self.w = torch.rand(input_size, requires_grad=train_mode)
+        self.b = torch.rand(1, requires_grad=train_mode)
 
     def forward(self, x):
         return torch.dot(self.w, x) - self.b
 
     def loss(self, x, y):
-        return max(0, 1 - y * self.forward(x))**2.0
+        return max(0, 1 - y * self.forward(x))**2.0 + self.epsilon * torch.dot(self.w, self.w)
 
     def get_parameters(self):
         return self.w, self.b
 
+    def train_mode(self, mode):
+        self.w.requires_grad_(mode)
+        self.b.requires_grad_(mode)
 
-class SVM_Tree:
-    def __init__(self, input_size, epsilon, classes):
+
+class SVMTree:
+    def __init__(self, input_size, epsilon, classes, learning_rate, train_mode=True):
         input_size = np.prod(input_size)
         self.svms = {}
         self.optimizers = {}
         for cls in classes:
-            self.svms[cls] = SVM(input_size, epsilon)
-            self.optimizers[cls] = torch.optim.SGD(self.svms[cls].get_parameters(), 0.01)
+            self.svms[cls] = SVM(input_size, epsilon, train_mode)
+            self.optimizers[cls] = torch.optim.SGD(self.svms[cls].get_parameters(), learning_rate)
         self.classes = classes
         self.epsilon = epsilon
 
     def step(self, x, y):
+        x = torch.tensor(x, dtype=torch.float32)
+        y = torch.tensor(y, dtype=torch.float32)
         for cls in self.classes:
             self.optimizers[cls].zero_grad()
             t = torch.tensor(1.0 if y == cls else 0.0)
@@ -53,16 +59,21 @@ class SVM_Tree:
         return losses
 
     def train(self, x_list, y_list, n_epochs, shuffle=True):
-        indexes = range(x_list)
-        losses = []
+        indexes = list(range(len(x_list[:, 0])))
         pbar = tqdm(range(n_epochs), unit='epochs', dynamic_ncols=True)
+        losses = []
         for i in pbar:
-            last_loss = None
+            total_loss = 0
             if shuffle:
                 random.shuffle(indexes)
             for index in tqdm(indexes, unit='steps', dynamic_ncols=True):
-                last_loss = self.step(x_list[index], y_list[index])
-            if last_loss is not None:
-                pbar.set_description(f'loss: {last_loss}')
-                losses.append(last_loss)
-        return losses
+                last_loss = self.step(x_list[index, :], y_list[index])
+                if last_loss is not None:
+                    total_loss += last_loss.data
+            loss_avg = total_loss.data[0] / len(indexes)
+            losses.append(loss_avg)
+            pbar.set_description(f'loss: {loss_avg}')
+
+    def train_mode(self, mode):
+        for cls in self.classes:
+            self.svms[cls].train_mode(mode)
--- run.py
+++ run.py
@@ -1,8 +1,22 @@
 import mnist
+from model import SVMTree
 
+# Download and get mnist
 training_images = mnist.train_images()
 test_images = mnist.test_images()
 training_labels = mnist.train_labels()
 test_labels = mnist.test_labels()
 
-print(training_images)
+# Set images to between 0 and 1
+training_images = training_images / 255.0
+test_images = test_images / 255.0
+
+# Flatten to 2 dimensions
+training_images = training_images.reshape((training_images.shape[0], training_images.shape[1] * training_images.shape[2]))
+test_images = test_images.reshape((test_images.shape[0], test_images.shape[1] * test_images.shape[2]))
+
+# Create svm tree
+svm = SVMTree(training_images.shape[1], 0.003, list(range(10)), 0.001)
+
+# Train svm
+svm.train(training_images, training_labels, 10)
