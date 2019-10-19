a630730ce9b02218b928746ac6a795627252f4b2
Armando Herrera <anhydrous99@gmail.com>
2019-10-19 00:49:59 -0500

    updates

--- model.py
+++ model.py
@@ -65,7 +65,7 @@ class SVMTree:
 
     def epoch(self, x_list, y_list):
         losses = []
-        for index, x in tqdm(enumerate(x_list), total=len(x_list), unit='steps', dynamic_ncols=True):
+        for index, x in tqdm(enumerate(x_list), total=len(x_list), unit='steps', dynamic_ncols=True, ascii=True):
             y = y_list[index]
             ls = self.step(x, y)
             if ls is not None:
@@ -74,19 +74,19 @@ class SVMTree:
 
     def train(self, x_list, y_list, n_epochs, shuffle=True):
         indexes = list(range(len(x_list[:, 0])))
-        pbar = tqdm(range(n_epochs), unit='epochs', dynamic_ncols=True)
+        pbar = tqdm(range(n_epochs), unit='epochs', dynamic_ncols=True, ascii=True)
         losses = []
         for i in pbar:
             total_loss = 0
             if shuffle:
                 random.shuffle(indexes)
-            for index in tqdm(indexes, unit='steps', dynamic_ncols=True):
+            for index in tqdm(indexes, unit='steps', dynamic_ncols=True, ascii=True):
                 last_loss = self.step(x_list[index, :], y_list[index])
                 if last_loss is not None:
                     total_loss += last_loss.data
             loss_avg = total_loss.data.item() / len(indexes)
             losses.append(loss_avg)
-            pbar.set_description(f'loss: {round(loss_avg, 4)}')
+            pbar.set_description(f'loss: {round(loss_avg, 2)}')
 
     def train_mode(self, mode):
         for cls in self.classes:
@@ -97,3 +97,12 @@ class SVMTree:
         for cls in self.classes:
             to_save[cls] = self.svms[cls].get_parameters(as_numpy)
         pickle.dump(to_save, open(path, 'wb'))
+
+    def evaluate(self, x, y):
+        correct = 0.0
+        count = x.shape[0]
+        for index in tqdm(range(count), desc='inference', ascii=True):
+            y_inf = self.inference(x[index])
+            if y_inf == y[index]:
+                correct += 1.0
+        return correct / count
--- run.py
+++ run.py
@@ -8,8 +8,8 @@ training_labels = mnist.train_labels()
 test_labels = mnist.test_labels()
 
 # Set images to between 0 and 1
-training_images = training_images / 255.0
-test_images = test_images / 255.0
+training_images = training_images / 127.5 - 1
+test_images = test_images / 127.5 - 1
 
 # Flatten to 2 dimensions
 training_images_flat = training_images.reshape((training_images.shape[0],
@@ -17,10 +17,9 @@ training_images_flat = training_images.reshape((training_images.shape[0],
 test_images_flat = test_images.reshape((test_images.shape[0], test_images.shape[1] * test_images.shape[2]))
 
 # Create svm tree
-svm = SVMTree(training_images_flat.shape[1], 0.003, list(range(10)), 0.0005)
+svm = SVMTree(training_images_flat.shape[1], 0.003, list(range(10)), 0.0001)
 
 # Train svm
-svm.train(training_images_flat, training_labels, 2)
-
-print(svm.inference(test_images_flat[4, :]))
-print(test_labels[4])
+svm.train(training_images_flat, training_labels, 100)
+svm.save_tree('SVM_tree.pickle')
+print(f'Accuracy: {round(svm.evaluate(test_images_flat, test_labels) * 100, 2)}%')
